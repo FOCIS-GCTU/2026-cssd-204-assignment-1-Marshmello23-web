d9dc836a0269dabce57132cbe7e6a1cc2db15da3
Joshua Gyamfi Ntiamoah <2425401784@live.gctu.edu.gh>
2026-05-29 15:03:40 +0000

    Finished Assignment 1
    Updated the main function to print stylized JGN initials with specific formatting.

--- initials.py
+++ initials.py
@@ -1,7 +1,30 @@
+# File: initials.py
+# Description: Prints stylized large JGN initials with specific formatting.
+# Assignment Number: 1
+#
+# Name: Joshua Gyamfi Ntiamoah
+# STUDENT ID: 2425401784
+# Email: 2425401784@live.gctu.edu.gh
+# Grader: Mr Augustus Buckman
+#
+# On my honor, Joshua Gyamfi Ntiamoah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
-
-
+    # This function prints the JGN initials layout to match requirements.
+    print()
+    print("...JGN")
+    print()
+    print("...JJJJJJJJJJJJ......GGGGGGGGGG......NNNN....NNNN...")
+    print("......JJJJJJ.........GGGGGGGGGGGG....NNNN....NNNN...")
+    print("......JJJJJJ.......GG..GGGGGGGG......NNNNN...NNNN...")
+    print("......JJJJJJ.......GG..GGGGGGGG......NNNNNN..NNNN...")
+    print("......JJJJJJ.......GG................NNNNNNN.NNNN...")
+    print("......JJJJJJ.......GG...GGGGGGG......NNNN.NNNNNNN...")
+    print("JJ....JJJJJJ.......GG...GGGG.GG......NNNN..NNNNNN...")
+    print("JJ....JJJJJJ.......GG..GGGG..GG**....NNNN...NNNNN**.")
+    print("JJJJJJJJJJJJ.......GGGGGGGGGGGG**....NNNN....NNNN**.")
+    print("..JJJJJJJJ..........GGGGGGGGGG.**.....NNNN....NNNN**.")
+    print()
 main()
